8413e20dfe3c571675ad43432372b4cffa8a44e4
ChrisTorng <christorng@hotmail.com>
2020-05-20 16:40:54 +0800

    Add Board.cs in GameOfLifeLibrary. Add GameOfLifeLibraryTests test project and default settings. Add BoardTests.cs.

---
+++ GameOfLifeLibrary/Board.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+namespace GameOfLife.Library
+{
+    public class Board
+    {
+        public Board(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+            this.Rows = new BitArray[height];
+        }
+
+        public int Width { get; }
+
+        public int Height { get; }
+
+#pragma warning disable CA1819 // Properties should not return arrays
+        public BitArray[] Rows { get; }
+#pragma warning restore CA1819 // Properties should not return arrays
+    }
+}
---
+++ GameOfLifeLibraryTests/BoardTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLife.Library.Tests
+{
+    [TestClass]
+    public class BoardTests
+    {
+        [TestMethod]
+        public void BoardTest()
+        {
+        }
+    }
+}
