a24723bd7b4c7a4ab91b521d9d32f1ddc4314af2
ChrisTorng <christorng@hotmail.com>
2020-05-29 16:18:41 +0800

    ImportWindow.xaml.cs adds LoadButton_Click implement.

--- GameOfLife/ImportWindow.xaml.cs
+++ GameOfLife/ImportWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Windows;
 using GameOfLife.Library;
+using Microsoft.Win32;
 
 namespace GameOfLife
 {
@@ -15,6 +18,19 @@ namespace GameOfLife
 
         private void LoadButton_Click(object sender, RoutedEventArgs e)
         {
+            var patternsPath = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "Patterns");
+            var fileDialog = new OpenFileDialog()
+            {
+                Filter = "Plaintext (*.cells)|*.cells",
+                InitialDirectory = patternsPath,
+            };
+
+            if (fileDialog.ShowDialog().Value)
+            {
+                this.ContentTextBox.Text = File.ReadAllText(fileDialog.FileName);
+            }
         }
 
         private void OkButton_Click(object sender, RoutedEventArgs e)
